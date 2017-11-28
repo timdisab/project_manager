468713cfed909d15c9d36a0e1e93160f087cbeb3
Tim DiSabatino <tim@pathwaysdigital.com>
2017-11-28 13:26:29 -0800

    Implement task destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,25 @@
 class TasksController < ApplicationController
   before_action :set_project
-  
+
   def create
-    @task = @project.task.create(task_params)
+    @task = @project.tasks.create(task_params)
+    redirect_to @project
+  end
+
+  def destroy
+    @task = @project.tasks.find(params[:id])
+    if @task.destroy
+      flash[:success] = "Task was deleted"
+    else
+      flash[:error] = "Task was not deleted"
+    end
     redirect_to @project
   end
 
   private
 
   def set_project
-    @project = Project.find(params[:id])
+    @project = Project.find(params[:project_id])
   end
 
   def task_params
