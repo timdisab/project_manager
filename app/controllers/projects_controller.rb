f8a49c76053c3dfcdb1cf0e0fbb21c2ccd309d5d
Tim DiSabatino <tim@pathwaysdigital.com>
2017-12-06 12:19:18 -0800

    Preliminary final styles

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -55,10 +55,7 @@ class ProjectsController < ApplicationController
   # DELETE /projects/1.json
   def destroy
     @project.destroy
-    respond_to do |format|
-      format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to root_url, notice: 'Project was successfully destroyed.'
   end
 
   private
